83bf5edd5a9fe28657a9223dca355c7785b5058f
Rmokgosi <58327526+Rmokgosi@users.noreply.github.com>
2020-11-12 13:57:32 +0200

    addingNew01
    adding new details 01

---
+++ assets/js/vending.js
@@ -0,0 +1,31 @@
+//declare variables
+var itemNo = new Array("1", "2", "3", "4", "5", "6", "7"); //item number array
+var itemPrice = new Array("11", "11", "12", "12", "8", "8", "9"); //price of item array
+
+var amt = Number(document.getElementById(amount).value); //inputAmount
+var itemNum = Number(document.getElementById(item_num).value);//inputItemNumber
+var price ;
+
+function getItemPrice()
+{  if () }
+
+
+// Clear field on a input box when clicking on the Refreah button
+function clearField(){
+	document.getElementById().value = ;
+}
+
+const reloadtButton = document.querySelector("#reload");
+
+
+
+function getItem(){
+
+}
+
+// Reload everything:
+function reload() {
+    reload = location.reload(); }
+/*Total amount = takes the for amount entered and subtract it from the inputAmount
+
+*/
